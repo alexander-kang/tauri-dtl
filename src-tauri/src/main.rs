e313a3a5ee005d58f6133286ea587493804f852f
alexander-kang <alexkang98767@gmail.com>
2023-04-23 01:18:39 -0500

    switch to prod

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -50,142 +50,142 @@ fn handle_form_submit(arr_labs: [bool; 5], src_path: &str, dst_path: &str) -> Pa
   let mut success_log_str: String = String::from("");
   let mut failure_log_str: String = String::from("");
 
-  // Testing code:
-  let rc: bool = fs_helper(src_path, dst_path);
-  if !rc {
-    res = false;
-    failure_log_str.push_str("test code");
-  } else {
-    success_log_str.push_str("test code");
+  // // Testing code:
+  // let rc: bool = fs_helper(src_path, dst_path);
+  // if !rc {
+  //   res = false;
+  //   failure_log_str.push_str("test code");
+  // } else {
+  //   success_log_str.push_str("test code");
+  // }
+
+  // Right half of template
+  let template_right: String = ".mcc.northwestern.edu\\".to_string();
+
+  // Check if Bodeen is being worked on
+  if arr_labs[0] {
+    // Left half of template for Bodeen
+    let bodeen_template_left: String = "\\\\bodeen-0".to_string();
+    // Copy files to all 5 of the Bodeen systems
+    for i in 1..6 {
+      let i_str: String = i.to_string();
+      // Put everything together to get the final destination path
+      let combined_bodeen_dst_path: String = format!("{}{}{}{}", bodeen_template_left, i_str, template_right, dst_path);
+      // Perform file operation
+      let rc: bool = fs_helper(src_path, combined_bodeen_dst_path.as_str());
+      // Update the return variables
+      if !rc {
+        res = false;
+        failure_log_str.push_str("bodeen-0");
+        failure_log_str.push_str(&i_str);
+        failure_log_str.push_str("<br>");
+      } else {
+        success_log_str.push_str("bodeen-0");
+        success_log_str.push_str(&i_str);
+        success_log_str.push_str("<br>");
+      }
+    }
   }
 
-  // // Right half of template
-  // let template_right: String = ".mcc.northwestern.edu\\".to_string();
+  // Check if MSE is being worked on
+  if arr_labs[1] {
+    // Left half of template for MSE
+    let mse_template_left: String = "\\\\mse-0".to_string();
+    // Copy files to all 7 of the MSE systems
+    for i in 1..8 {
+      let i_str: String = i.to_string();
+      // Put everything together to get the final destination path
+      let combined_mse_dst_path: String = format!("{}{}{}{}", mse_template_left, i_str, template_right, dst_path);
+      // Perform file operation
+      let rc: bool = fs_helper(src_path, combined_mse_dst_path.as_str());
+      // Update the return variables
+      if !rc {
+        res = false;
+        failure_log_str.push_str("mse-0");
+        failure_log_str.push_str(&i_str);
+        failure_log_str.push_str("<br>");
+      } else {
+        success_log_str.push_str("mse-0");
+        success_log_str.push_str(&i_str);
+        success_log_str.push_str("<br>");
+      }
+    }
+  }
 
-  // // Check if Bodeen is being worked on
-  // if arr_labs[0] {
-  //   // Left half of template for Bodeen
-  //   let bodeen_template_left: String = "\\\\bodeen-0".to_string();
-  //   // Copy files to all 5 of the Bodeen systems
-  //   for i in 1..6 {
-  //     let i_str: String = i.to_string();
-  //     // Put everything together to get the final destination path
-  //     let combined_bodeen_dst_path: String = format!("{}{}{}{}", bodeen_template_left, i_str, template_right, dst_path);
-  //     // Perform file operation
-  //     let rc: bool = fs_helper(src_path, combined_bodeen_dst_path.as_str());
-  //     // Update the return variables
-  //     if !rc {
-  //       res = false;
-  //       failure_log_str.push_str("bodeen-0");
-  //       failure_log_str.push_str(&i_str);
-  //       failure_log_str.push_str("<br>");
-  //     } else {
-  //       success_log_str.push_str("bodeen-0");
-  //       success_log_str.push_str(&i_str);
-  //       success_log_str.push_str("<br>");
-  //     }
-  //   }
-  // }
+  // Check if ChBe is being worked on
+  if arr_labs[2] {
+    // Left half of template for ChBe
+    let chbe_template_left: String = "\\\\e1-chbe-0".to_string();
+    // Copy files to all 8 of the ChBe systems
+    for i in 1..9 {
+      let i_str: String = i.to_string();
+      // Put everything together to get the final destination path
+      let combined_chbe_dst_path: String = format!("{}{}{}{}", chbe_template_left, i_str, template_right, dst_path);
+      // Perform file operation
+      let rc: bool = fs_helper(src_path, combined_chbe_dst_path.as_str());
+      // Update the return variables
+      if !rc {
+        res = false;
+        failure_log_str.push_str("e1-chbe-0");
+        failure_log_str.push_str(&i_str);
+        failure_log_str.push_str("<br>");
+      } else {
+        success_log_str.push_str("e1-chbe-0");
+        success_log_str.push_str(&i_str);
+        success_log_str.push_str("<br>");
+      }
+    }
+  }
 
-  // // Check if MSE is being worked on
-  // if arr_labs[1] {
-  //   // Left half of template for MSE
-  //   let mse_template_left: String = "\\\\mse-0".to_string();
-  //   // Copy files to all 7 of the MSE systems
-  //   for i in 1..8 {
-  //     let i_str: String = i.to_string();
-  //     // Put everything together to get the final destination path
-  //     let combined_mse_dst_path: String = format!("{}{}{}{}", mse_template_left, i_str, template_right, dst_path);
-  //     // Perform file operation
-  //     let rc: bool = fs_helper(src_path, combined_mse_dst_path.as_str());
-  //     // Update the return variables
-  //     if !rc {
-  //       res = false;
-  //       failure_log_str.push_str("mse-0");
-  //       failure_log_str.push_str(&i_str);
-  //       failure_log_str.push_str("<br>");
-  //     } else {
-  //       success_log_str.push_str("mse-0");
-  //       success_log_str.push_str(&i_str);
-  //       success_log_str.push_str("<br>");
-  //     }
-  //   }
-  // }
+  // Check if Segal is being worked on
+  if arr_labs[3] {
+    // Left half of template for Segal
+    let segal_template_left: String = "\\\\e1-segal-0".to_string();
+    // Copy files to all 7 of the Segal systems
+    for i in 1..8 {
+      let i_str: String = i.to_string();
+      // Put everything together to get the final destination path
+      let combined_segal_dst_path: String = format!("{}{}{}{}", segal_template_left, i_str, template_right, dst_path);
+      // Perform file operation
+      let rc: bool = fs_helper(src_path, combined_segal_dst_path.as_str());
+      // Update the return variables
+      if !rc {
+        res = false;
+        failure_log_str.push_str("e1-segal-0");
+        failure_log_str.push_str(&i_str);
+        failure_log_str.push_str("<br>");
+      } else {
+        success_log_str.push_str("e1-segal-0");
+        success_log_str.push_str(&i_str);
+        success_log_str.push_str("<br>");
+      }
+    }
+  }
 
-  // // Check if ChBe is being worked on
-  // if arr_labs[2] {
-  //   // Left half of template for ChBe
-  //   let chbe_template_left: String = "\\\\e1-chbe-0".to_string();
-  //   // Copy files to all 8 of the ChBe systems
-  //   for i in 1..9 {
-  //     let i_str: String = i.to_string();
-  //     // Put everything together to get the final destination path
-  //     let combined_chbe_dst_path: String = format!("{}{}{}{}", chbe_template_left, i_str, template_right, dst_path);
-  //     // Perform file operation
-  //     let rc: bool = fs_helper(src_path, combined_chbe_dst_path.as_str());
-  //     // Update the return variables
-  //     if !rc {
-  //       res = false;
-  //       failure_log_str.push_str("e1-chbe-0");
-  //       failure_log_str.push_str(&i_str);
-  //       failure_log_str.push_str("<br>");
-  //     } else {
-  //       success_log_str.push_str("e1-chbe-0");
-  //       success_log_str.push_str(&i_str);
-  //       success_log_str.push_str("<br>");
-  //     }
-  //   }
-  // }
-
-  // // Check if Segal is being worked on
-  // if arr_labs[3] {
-  //   // Left half of template for Segal
-  //   let segal_template_left: String = "\\\\e1-segal-0".to_string();
-  //   // Copy files to all 7 of the Segal systems
-  //   for i in 1..8 {
-  //     let i_str: String = i.to_string();
-  //     // Put everything together to get the final destination path
-  //     let combined_segal_dst_path: String = format!("{}{}{}{}", segal_template_left, i_str, template_right, dst_path);
-  //     // Perform file operation
-  //     let rc: bool = fs_helper(src_path, combined_segal_dst_path.as_str());
-  //     // Update the return variables
-  //     if !rc {
-  //       res = false;
-  //       failure_log_str.push_str("e1-segal-0");
-  //       failure_log_str.push_str(&i_str);
-  //       failure_log_str.push_str("<br>");
-  //     } else {
-  //       success_log_str.push_str("e1-segal-0");
-  //       success_log_str.push_str(&i_str);
-  //       success_log_str.push_str("<br>");
-  //     }
-  //   }
-  // }
-
-  // // Check if MCC is being worked on
-  // if arr_labs[4] {
-  //   // Left half of template for MCC
-  //   let mcc_template_left: String = "\\\\e1-mcc-0".to_string();
-  //   // Copy files to all 26 of the MCC systems
-  //   for i in 1..27 {
-  //     let i_str: String = i.to_string();
-  //     // Put everything together to get the final destination path
-  //     let combined_mcc_dst_path: String = format!("{}{}{}{}", mcc_template_left, i_str, template_right, dst_path);
-  //     // Perform file operation
-  //     let rc: bool = fs_helper(src_path, combined_mcc_dst_path.as_str());
-  //     // Update the return variables
-  //     if !rc {
-  //       res = false;
-  //       failure_log_str.push_str("e1-mcc-0");
-  //       failure_log_str.push_str(&i_str);
-  //       failure_log_str.push_str("<br>");
-  //     } else {
-  //       success_log_str.push_str("e1-mcc-0");
-  //       success_log_str.push_str(&i_str);
-  //       success_log_str.push_str("<br>");
-  //     }
-  //   }
-  // }
+  // Check if MCC is being worked on
+  if arr_labs[4] {
+    // Left half of template for MCC
+    let mcc_template_left: String = "\\\\e1-mcc-0".to_string();
+    // Copy files to all 26 of the MCC systems
+    for i in 1..27 {
+      let i_str: String = i.to_string();
+      // Put everything together to get the final destination path
+      let combined_mcc_dst_path: String = format!("{}{}{}{}", mcc_template_left, i_str, template_right, dst_path);
+      // Perform file operation
+      let rc: bool = fs_helper(src_path, combined_mcc_dst_path.as_str());
+      // Update the return variables
+      if !rc {
+        res = false;
+        failure_log_str.push_str("e1-mcc-0");
+        failure_log_str.push_str(&i_str);
+        failure_log_str.push_str("<br>");
+      } else {
+        success_log_str.push_str("e1-mcc-0");
+        success_log_str.push_str(&i_str);
+        success_log_str.push_str("<br>");
+      }
+    }
+  }
 
   return Payload {
     result: res,
